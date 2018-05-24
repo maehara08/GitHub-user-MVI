f33bfa98822ed31e8e218852c3e24ecf544e0b42
Riku Maehara <aso.riiiku08@gmail.com>
2018-05-24 22:03:30 +0900

    Init commit.

---
+++ app/src/androidTest/java/maehara08/github_user_mvi/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package maehara08.github_user_mvi
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("maehara08.github_user_mvi", appContext.packageName)
+    }
+}
