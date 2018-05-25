b319ede76930c18fe9d14c1bcce3e887d233ee69
maehara08 <aso.riiiku08@gmail.com>
2018-05-25 16:06:48 +0900

    Add users act.

---
+++ app/src/main/java/maehara08/github_user_mvi/users/UsersActivity.kt
@@ -0,0 +1,13 @@
+package maehara08.github_user_mvi.users
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import maehara08.github_user_mvi.R
+
+class UsersActivity() : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.users_activity)
+    }
+}
