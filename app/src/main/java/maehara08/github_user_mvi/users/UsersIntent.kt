7e589ae48e510dea8124108c92597332d2628022
maehara08 <aso.riiiku08@gmail.com>
2018-05-28 15:12:44 +0900

    Add users intent.

---
+++ app/src/main/java/maehara08/github_user_mvi/users/UsersIntent.kt
@@ -0,0 +1,17 @@
+package maehara08.github_user_mvi.users
+
+import maehara08.github_user_mvi.mvibase.MviIntent
+
+sealed class UsersIntent : MviIntent {
+    object InitialIntent : UsersIntent()
+
+    data class RefreshIntent(val forceUpdate: Boolean) : UsersIntent()
+
+//    data class ActivateTaskIntent(val user: User) : UsersIntent()
+
+//    data class CompleteTaskIntent(val user: User) : UsersIntent()
+
+//    object ClearCompletedUsersIntent : UsersIntent()
+
+//    data class ChangeFilterIntent(val filterType: TasksFilterType) : UsersIntent()
+}
